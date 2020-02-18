29b9556a68b3a782996a8c1f9559bbea9b64113c
Wei Xin <cheewx@hotmail.com>
2020-02-18 21:26:32 +0800

    added modulelist (not final)

---
+++ src/Pages/ModuleList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="md-layout">
+    <div class="md-layout-item md-size-5"></div>
+    <div class="md-layout-item md-size-20">
+      <md-content class="md-scrollbar">
+        <table>
+          <tr>
+            <td style="width:60%; font-size:130%">Filter By :</td>
+            <td style="color:red">Clear Filters</td>
+          </tr>
+        </table>
+        <span class="minihead">Offered in</span>
+        <v-checkbox></v-checkbox>
+      </md-content>
+    </div>
+    <div class="md-layout-item"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss" scoped>
+@import "~vue-material/src/theme/engine";
+
+.md-layout-item {
+  height: 1000px;
+
+  &:nth-child(2) {
+    background: md-get-palette-color(grey, 50);
+  }
+}
+.md-content {
+  max-width: 400px;
+  max-height: 200px;
+  overflow: auto;
+  padding: 20px;
+  font-family: Helvetica;
+}
+
+.md-checkbox {
+  display: flex;
+}
+.minihead {
+  font-size: 80%;
+  font-weight: bold;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { MdButton, MdContent, MdToolbar, MdTabs} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import vuetify from './plugins/vuetify';
+
+Vue.use(VueMaterial);
 Vue.use(MdButton)
 Vue.use(MdContent)
 Vue.use(MdToolbar)
@@ -15,6 +21,7 @@ Vue.use(MdTabs)
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify
   render: h => h(app),
   el: '#app',
 }).$mount('#app')
