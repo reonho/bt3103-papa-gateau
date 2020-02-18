884bdeb0d5fd71bba218b66ef49dd9643830e9c0
sgxcj777 <lamcj97@gmail.com>
2020-02-18 09:38:35 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import Vue from 'vue'
 import app from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import VueApexCharts from 'vue-apexcharts'
 Vue.config.productionTip = false
-Vue.component('apexchart', VueApexCharts)
-
 
 new Vue({
   render: h => h(app),
