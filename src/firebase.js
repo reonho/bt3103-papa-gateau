0113c105bf8053a833866d650a5e59f254ad4a60
Rance <limjyrance@gmail.com>
2020-04-05 15:17:19 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -549,25 +549,6 @@ var database = {
 
   },
 
-  //=====================================//
-  //----------- getFaculties-------------//
-  //=====================================//
-  async getFaculties(){
-    var promise = new Promise(resolve => {
-      var faculties = [];
-      database.firebase_data.collection("faculties")
-      .get().then(snapshot => {
-        snapshot.forEach(doc =>{
-          faculties.push(doc.data().name)
-        })
-        faculties.sort()
-        resolve(faculties)
-      })
-    })
-    return promise
-  },
-  
-
 }
 
 
