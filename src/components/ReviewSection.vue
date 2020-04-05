24b96f86862afa05ca4b18e2297a2d57aa212076
Rance <limjyrance@gmail.com>
2020-04-05 22:32:07 +0800

    added empty state

--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="ReviewSection">
-    <div v-for="r in reviewData" v-bind:key="'review'+r.id">
-        <ReviewCard :review='r'></ReviewCard>
+  <div>
+    <div id="ReviewSection" v-if='hasReviews===true'>
+      <div v-for="r in reviewData" v-bind:key="'review'+r.id" >
+        <ReviewCard :review="r"></ReviewCard>
+      </div>
+    </div>
+    <div class = 'empty-state' id = 'EmptyState' v-if='hasReviews===false'>
+      <md-empty-state
+        class ='md-accent'
+        md-icon='post_add'
+        md-label="No Reviews"
+        md-description="There are no reviews yet. Write a review now!"
+      />
     </div>
   </div>
 </template>
@@ -16,45 +26,37 @@ export default {
   props: {
     userid: String,
     mod: String,
-    reviewData: Array,
-  }, 
+    reviewData: Array
+  },
   // {
   //   reviewData: Array //should be an array of reviews from the backend
   // },
   data: () => ({
+    hasReviews: true
     // reviewData: DataObject.reviewData
     // reviewData: [],
     //userid: 'e0123451'
   }),
 
   components: {
-    ReviewCard,
+    ReviewCard
   },
 
-  created() {
-    // if (this.userid != null){
-    //   database.getUserReview(this.userid).then(reviews=>{
-    //     this.reviewData = reviews
-    //     console.log(this.userid)
-    //     console.log(this.reviewData)
-    //   })
-    // } else if (this.mod != null){
-    //   database.getModuleReviewID(this.mod).then(reviews =>{
-    //     this.reviewData = reviews
-    //     console.log(this.mod)
-    //     console.log(this.reviewData[0])
-    //   })
-    // }
-    //query all reviews written by user
-     // console.log(this.reviewData)
-      
-    
-
+  updated() {
+    console.log("ReviewSection " + this.reviewData)
+    if (this.reviewData.length > 0) {
+      this.hasReviews = true;
+    } else {
+      this.hasReviews = false;
+    }
   }
 };
 </script>
 
 
 <style scoped>
+.empty-state {
+  /* color:  #17a2b8 */
+}
 </style>
 
