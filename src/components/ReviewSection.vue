323e3634525480d6285924c41a3337674217cff4
Rance <limjyrance@gmail.com>
2020-04-19 16:46:09 +0800

    empty state

--- src/components/ReviewSection.vue
+++ src/components/ReviewSection.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
-    <div id="ReviewSection" v-if="hasReviews===true">
+    <div id="ReviewSection" v-show="hasReviews===true && dataLoaded">
       <div v-for="r in reviewData" v-bind:key="'review'+r.id">
         <ReviewCard :review="r"></ReviewCard>
       </div>
     </div>
-    <div class="empty-state" id="EmptyState" v-if="hasReviews===false">
+    <div class="empty-state" id="EmptyState" v-show="hasReviews===false && dataLoaded">
       <md-empty-state
-        class="md-accent"
+        style="color: #2e4053 !important;"
         md-icon="post_add"
         md-label="No Reviews"
         md-description="There are no reviews yet. Write a review now!"
       />
     </div>
+    <!-- <div class="loading-state" id="LoadingState" v-show="loading">
+      <md-empty-state
+        style="max-width:0 !important; margin-top:-2vw; margin-bottom:5vw;color: #2e4053;"
+        md-label="Loading..."
+      />
+      <ScaleLoader :loading="loading" :color="color" :size="size"></ScaleLoader>
+    </div> -->
   </div>
 </template>
 
@@ -20,6 +27,7 @@
 <script>
 // import DataObject from "../Database.js";
 import ReviewCard from "./ReviewCard";
+// import ScaleLoader from "vue-spinner/src/ScaleLoader.vue";
 // import database from '../firebase.js';
 export default {
   name: "ReviewSection",
@@ -28,18 +36,56 @@ export default {
     mod: String,
     reviewData: Array
   },
+  methods: {
+    setReviewStatus() {
+      if (this.reviewData.length > 0) {
+        this.hasReviews = true;
+      } else {
+        this.hasReviews = false;
+      }
+      // while (this.dataLoaded === false) {
+      //   this.shortload(2000);
+      //   console.log(this.reviewData);
+      //   if (this.reviewData !== undefined && this.reviewData !== null) {
+      //     this.dataLoaded = true;
+      //   }
+      // }
+      // if (this.dataLoaded === true) {
+
+      // }
+
+      // this.$root.$emit("setReviewStatus");
+    },
+    shortload: function(n) {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, n);
+    }
+    // showloading: function() {
+    //   this.loading = true;
+    //   this.$root.$on("setReviewStatus", function() {
+    //     setTimeout(() => {
+    //       this.loading = false;
+    //     }, 800);
+    //   });
+    // }
+  },
   // {
   //   reviewData: Array //should be an array of reviews from the backend
   // },
   data: () => ({
-    hasReviews: true
+    loading: true,
+    hasReviews: false,
+    dataLoaded: false
     // reviewData: DataObject.reviewData
     // reviewData: [],
     //userid: 'e0123451'
   }),
 
   components: {
-    ReviewCard
+    ReviewCard,
+    // ScaleLoader
   },
   // created() {
   //   console.log("ReviewSection " + this.reviewData)
@@ -48,23 +94,27 @@ export default {
   //   } else {
   //     this.hasReviews = false;
   //   }
-   
   // },
-
-  beforeUpdated() {
-    console.log("ReviewSection " + this.reviewData)
-    if (this.reviewData.length > 0) {
-      this.hasReviews = true;
-    } else {
-      this.hasReviews = false;
+  created() {
+    // this.setReviewStatus();
+  },
+  watch: {
+    reviewData() {
+      console.log('watch')
+      this.setReviewStatus();
+      this.dataLoaded = true
+      this.loading = false
     }
   }
+
+  // }
 };
 </script>
 
 
 <style scoped>
 .empty-state {
+  color: #2e4053;
   /* color:  #17a2b8 */
 }
 </style>
